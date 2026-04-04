db3f326c7e52d857510f72446bc1c66eb9c0a3da
CODECZERO <as8857981@gmail.com>
2026-04-04 08:12:46 +0530

    fix(frontend): update next.config.mjs header source matcher to resolve widespread vercel 404s

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -15,7 +15,7 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: '/(.*)',
+        source: '/:path*',
         headers: [
           {
             key: 'Content-Security-Policy',
